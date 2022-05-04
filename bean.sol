1644e767ea4cfdc38933c01908ab57490edfc836
Navraj <navraj2071@gmail.com>
2022-05-04 13:01:44 +0530

    Updated bean contract to be capped.

--- bean.sol
+++ bean.sol
@@ -1,13 +1,34 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
 
-contract Bean is ERC20Burnable  {
-      
-    constructor(uint256 initialSupply) ERC20("bean", "bne") {
-        _mint(msg.sender, initialSupply);
-    }   
+contract Bean is ERC20Capped {  
+    uint256 public maxSupply = 100000000000; 
+    address public owner;  
+
+    //events
+    event beansCreated (uint256 _beans);
+    event beansBurnt (uint256 _beans);
+
+
+    constructor(uint256 initialsupply) ERC20("bean", "bne") ERC20Capped(maxSupply) {
+        ERC20._mint(msg.sender, initialsupply);
+        owner = msg.sender;        
+    }  
+
+    function mintBean(uint256 _beans) public {
+        require(msg.sender == owner, "Only owner can mint new tokens.");
+        _mint(msg.sender, _beans);
+        emit beansCreated(_beans);
+    }     
+
+   function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount);
+        emit beansBurnt(amount);
+    }
 
 }
